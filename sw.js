2b8786b95ffaef597d2c1a2e41fabb3c1d883bd9
jeffersonjpo <jefferson_jpo@hotmail.com>
2026-05-16 04:08:55 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE='fin-v2';
+self.addEventListener('install',e=>{self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(fetch(e.request).then(res=>{
+    if(res&&res.status===200&&res.type==='basic'){const clone=res.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));}
+    return res;
+  }).catch(()=>caches.match(e.request)));
+});
